3354b71c870e5b006ef1ba9df4703c3c2d01d17f
Sanjida Akter <sanjidatanha09@gmail.com>
2024-04-17 08:45:16 +0600

    space removed

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -5,48 +5,25 @@ import banner from "../../public/Image/Rectangle 8.png"
 const Banner = () => {
     return (
         <div>
-
             <div className="carousel w-full h-[400px] md:h-[500px] lg:h-[600px] rounded-3xl mt-10">
-
                 <div id="slide1" className="carousel-item relative w-full ">
                     <img src={banner} className="w-[95%] mx-auto md:w-[100%] rounded-3xl" />
                     <div className="absolute flex items-center  h-full left-0  top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] rounded-3xl">
                         <div className='text-white space-y-3 pl-24 w-[300px] md:w-[500px]  lg:w-[70%] my-10 '>
                             <h1 className='text-xl md:text-3xl lg:text-4xl font-bold'>CMA Fest</h1>
                             <p className='text-sm md:text-lg lg:text-xl pb-8'>Make Memories That Will Last A Lifetime.See Your Favorite Artists Live At CMA Fest!</p>
-
                             <div className='flex gap-4 mt-5 text-white'>
                                 <button className='btn bg-[#A020F0] text-white text-xs lg:text-xl'>Get Ticket</button>
                                 <button className='btn bg-ghost text-white  btn-outline text-xs  lg:text-xl'>Explore All Events</button>
-
                             </div>
-
                         </div>
                     </div>
-
                     <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 bottom-0 gap-5">
                         <a href="#slide2" className=" bg-white text-white w-[10px] h-[10px] rounded-full"></a>
                         <a href="#slide2" className=" bg-white text-white w-[10px] h-[10px] rounded-full"></a>
                         <a href="#slide2" className=" bg-white text-white w-[10px] h-[10px] rounded-full"></a>
                     </div>
-
                 </div>
-
-
-
-                
-
-
-
-
-
-
-
-
-
-
-
-
             </div>
         </div>
     );
--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -10,7 +10,6 @@ import Watch from "../../public/Image/Group.png"
 const Events = () => {
     return (
         <div className='w-[95%] md:w-[100%] mx-auto'>
-
             <div className='mt-10'>
                 <h1 className='font-bold  text-black text-center text-4xl'>Events</h1>
                 <div className='grid grid-cols-3 md:flex justify-center gap-1 md:gap-5 mt-5 md:items-start  items-center w-[90%] mx-auto md:w-full'>
@@ -20,10 +19,8 @@ const Events = () => {
                     <p className='font-medium text-base text-black hover-element'>This Week</p>
                     <p className='font-medium text-base text-black hover-element'>Music</p>
                     <p className='font-medium text-base text-black hover-element text-center'>Union</p>
-
                 </div>
                 <div className='mt-5 flex flex-col lg:flex-row items-center justify-between gap-4'>
-
                     <div className="card md:w-[100%] lg:w-[30%] bg-base-100 shadow-xl gap-2">
                         <div className='relative'>
                             <img src={img2} alt="Shoes" className='p-3 w-full  border-b border-dashed h-[270px]' />
@@ -32,10 +29,8 @@ const Events = () => {
                         </div>
                         <div className="mt-3 pl-3">
                             <div className='flex justify-between pr-4'>
-
                                 <div>
                                     <div className='flex justify-between items-start'>
-
                                         <p className='bg-violet-600 w-[55px] h-[30px] text-center pt-1 text-white font-medium rounded-lg text-sm mb-2 '>Music</p>
                                     </div>
                                     <div className='mb-2'>
@@ -54,11 +49,8 @@ const Events = () => {
                                 <div className='flex  bg-white shadow-lg mx-auto w-[80px]'>
                                     <p className=' text-violet-600 font-bold pl-5 text-lg'>20</p>
                                     <p className='font-medium text-[8px]'>Seat  <br />left</p>
-
                                 </div>
                                 <button className='text-violet-600 w-[120px] h-[30px] text-center py-1  font-medium rounded-lg md:text-base lg:text-base xl:text-lg '>See Ticket</button>
-
-
                             </div>
                         </div>
                     </div>
@@ -70,10 +62,8 @@ const Events = () => {
                         </div>
                         <div className="mt-3 pl-3">
                             <div className='flex justify-between pr-4'>
-
                                 <div>
                                     <div className='flex justify-between items-start'>
-
                                         <p className='bg-violet-600 w-[55px] h-[30px] text-center pt-1 text-white font-medium rounded-lg text-sm mb-2 '>Music</p>
                                     </div>
                                     <div className='mb-2'>
@@ -92,11 +82,8 @@ const Events = () => {
                                 <div className='flex  bg-white shadow-lg mx-auto w-[80px]'>
                                     <p className=' text-violet-600 font-bold pl-5 text-lg'>20</p>
                                     <p className='font-medium text-[8px]'>Seat  <br />left</p>
-
                                 </div>
                                 <button className='text-violet-600 w-[120px] h-[30px] text-center py-1  font-medium rounded-lg md:text-base lg:text-base xl:text-lg '>See Ticket</button>
-
-
                             </div>
                         </div>
                     </div>
@@ -108,10 +95,8 @@ const Events = () => {
                         </div>
                         <div className="mt-3 pl-3">
                             <div className='flex justify-between pr-4'>
-
                                 <div>
                                     <div className='flex justify-between items-start'>
-
                                         <p className='bg-violet-600 w-[55px] h-[30px] text-center pt-1 text-white font-medium rounded-lg text-sm mb-2 '>Music</p>
                                     </div>
                                     <div className='mb-2'>
@@ -130,61 +115,43 @@ const Events = () => {
                                 <div className='flex  bg-white shadow-lg mx-auto w-[80px]'>
                                     <p className=' text-violet-600 font-bold pl-5 text-lg'>20</p>
                                     <p className='font-medium text-[8px]'>Seat  <br />left</p>
-
                                 </div>
                                 <button className='text-violet-600 w-[120px] h-[30px] text-center py-1  font-medium rounded-lg md:text-base lg:text-base xl:text-lg '>See Ticket</button>
-
-
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             {/* bg-gradient-to-r from-cyan-500 to-blue-500 */}
-
             {/* 2nd part */}
-
-            <div className='flex flex-col lg:flex-row mt-10 gap-2 bg-violet-600 rounded-3xl justify-between items-start'> 
-
+            <div className='flex flex-col lg:flex-row mt-10 gap-2 bg-violet-600 rounded-3xl justify-between items-start'>
                 <div className='w-[100%] lg:w-[50%]'>
                     <img className='lg:p-10 lg:w-[90%] mx-auto h-[400px]' src={Host} alt="" />
                 </div>
                 <div className=' text-white w-[100%] lg:w-[50%] mx-auto relative'>
                     <h1 className='px-5 md:px-10 pt-10 font-bold text-xl pb-4 w-full mx-auto'>Host Your Event Easily & Automatedly With Us</h1>
                     <h5 className='px-10 text-base font-bold pb-4'>We Provide The Best Event Ticketing & Management Solutions</h5>
-
                     <div className='px-10 flex items-start pb-2'>
                         <img src="https://i.ibb.co/4TbJQ7V/pngtree-checkmark-right-icon-tick-circle-png-image-4647427.png" alt="" className='rounded-full w-[20px]' />
                         <p className='text-sm pl-2'>Host your Event With Easy Steps!</p>
-
                     </div>
                     <div className='px-10 flex  items-start pb-2'>
                         <img src="https://i.ibb.co/4TbJQ7V/pngtree-checkmark-right-icon-tick-circle-png-image-4647427.png" alt="" className='rounded-full w-[20px]' />
                         <p className='text-sm pl-2'>Automated Payment System & Ticket Will Be Sent To Mall!</p>
-
                     </div>
                     <div className='px-10 flex items-start pb-2'>
                         <img src="https://i.ibb.co/4TbJQ7V/pngtree-checkmark-right-icon-tick-circle-png-image-4647427.png" alt="" className='rounded-full w-[20px]' />
                         <p className='text-sm pl-2'>Access To Registration And Transaction Reports</p>
-
                     </div>
-
                     <div className='px-10 flex items-start pb-24 md:pb-3'>
                         <img src="https://i.ibb.co/4TbJQ7V/pngtree-checkmark-right-icon-tick-circle-png-image-4647427.png" alt="" className='rounded-full w-[20px]' />
                         <p className='text-sm pl-2'>Send Bulk Personalized Mail With Attachments.</p>
-
                     </div>
                     <div className='w-[140px] md:w-[200px] lg:w-[300px] absolute top-50 bottom-0 md:top-12 lg:top-28 right-0 p-0 '>
-
                         <img className='w-[100%]  text-end ' src={Watch} alt="" />
-
                     </div>
-                    
                 </div>
-              
-
             </div>
-
         </div>
     );
 };
